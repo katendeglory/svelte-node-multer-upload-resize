d7ddbfd21758b633073a46c914240e310170949e
Glory Katende <katendeglory@gmail.com>
2020-08-18 16:54:05 +0200

    Yet another commit

---
+++ client/src/App.svelte
@@ -0,0 +1,62 @@
+<script>
+  import Resizer from "react-image-file-resizer";
+  import axios from "axios";
+  const resize = Resizer.imageFileResizer;
+
+  let rawImgs;
+  let resizedImgs;
+  let description;
+
+  let resizeImage = (file) => {
+    return new Promise((resolve, reject) => {
+      resize(
+        file,
+        500,
+        500,
+        "JPEG",
+        100,
+        0,
+        // (uri) => resolve(new File([uri], file.name)),
+        (uri) => resolve(uri),
+        "blob"
+      );
+    });
+  };
+
+  let resizeImages = async (files) => {
+    let resized = [];
+    for (let i = 0; i < files.length; i++) {
+      resized.push(await resizeImage(files[i]));
+    }
+    return resized;
+  };
+
+  let onSubmit = async () => {
+    resizedImgs = await resizeImages(rawImgs);
+
+    let formData = new FormData();
+
+    resizedImgs.forEach((resizedImg) => formData.append("images", resizedImg));
+
+    formData.append("description", description);
+
+    axios
+      .post("http://localhost:5001/upload", formData)
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  };
+</script>
+
+<h1>Fill this form!</h1>
+
+<input type="text" bind:value={description} />
+<br />
+<br />
+<input type="file" bind:files={rawImgs} multiple />
+<br />
+<br />
+<button on:click={onSubmit}>Submit</button>
---
+++ server/server.js
@@ -0,0 +1,62 @@
+//MAIN IMPORTS
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+//MIDDLEWARES
+app.use(cors());
+app.use(express.static('public'));
+
+//MULTER
+const multer = require('multer');
+
+//STORAGE ENGINE
+const storage = multer.diskStorage({
+  destination: "uploaded", /* Must Exist */
+  filename: (req, file, cb) => {
+    const fieldname = file.fieldname;
+    const extension = file.mimetype.split("/")[1] || "";
+    const prefix = Date.now();
+    const destFileName = fieldname + "-" + prefix + "." + extension;
+    // console.log(destFileName);
+    cb(null, destFileName);
+  }
+});
+
+
+//UPLOAD ENGINE
+const upload = multer({
+  storage: storage,
+  /* 
+    The Filter Runs for every single file in the upload engine, operation either collectively succed or fails (atomic). 
+    Being a middleware, it can definitely affect req.body of elements coming before it (Order matters 😀).
+  */
+  fileFilter: (req, file, cb) => {
+    // console.log("\n⭐ YET A NEW FILE UPLOADED ⭐\n");
+    // console.log(file.mimetype);
+    if (!file.mimetype.includes('image')) cb(new Error('The Server Only Accept Image Files'), false);
+    else cb(null, true);
+  },
+  // limits: {
+  //   fileSize: 2_097_152 //2MB
+  // }
+});
+
+
+//ROUTES
+app.post('/upload', upload.fields([{ name: 'images', maxCount: 8 }]), (req, res) => {
+  res.send({ file: req.file, files: req.files, body: req.body });
+});
+
+
+//Doing Express Handling on Our Own
+app.post('/upload-2', (req, res) => {
+  upload(req, res, (err) => {
+    if (err) res.send(err);
+    else res.send("Upload Successfull");
+  });
+});
+
+//SERVER
+const port = process.env.PORT || 5001;
+app.listen(port, () => console.log(`\n\n⚡ Listening on Port ${port}\n\n`));
